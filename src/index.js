92c303a3f73fde56ddbeb4ba64a52c608a3ea245
Nicolas Kloster <niko.klos@gmail.com>
2022-10-19 20:42:42 -0300

    fix detalles

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
 
 */
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "assets/vendor/nucleo/css/nucleo.css";
@@ -25,13 +25,12 @@ import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
 import Index from "views/Landing.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact render={(props) => <Index {...props} />} />
       <Redirect to="/" />
     </Switch>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
